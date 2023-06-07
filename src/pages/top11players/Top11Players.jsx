a9d21a311616d43056e71455997b71b9960a0eb7
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-06-07 20:05:55 +0200

    top 11 done, Preguntar a jorge sobre profile

--- src/pages/top11players/Top11Players.jsx
+++ src/pages/top11players/Top11Players.jsx
@@ -11,7 +11,7 @@ function Top11Players() {
   const [defenseCount, setDefenseCount] = useState(0);
   const [midfielderCount, setMidfielderCount] = useState(0);
   const [forwardCount, setForwardCount] = useState(0);
-  const [ noTop11, setNoTop11 ] = useState([])
+  const [noTop11, setNoTop11] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,21 +23,19 @@ function Top11Players() {
       const response = await playersListService();
       console.log(response, "response");
       setPlayersList(response.data);
-      const top11response = await getTop11Service()
-      console.log(top11response)
-      setTop11PlayersList(top11response.data)
+      const top11response = await getTop11Service();
+      console.log(top11response);
+      setTop11PlayersList(top11response.data);
       const userInfo = await profileService();
-      console.log(userInfo, "userinfo")
-      const remainingPlayers = response.data.filter(player => {
-        return !userInfo.data.top11.includes(player._id)
-      })
-      setNoTop11(remainingPlayers)
+      console.log(userInfo, "userinfo");
+      const remainingPlayers = response.data.filter((player) => {
+        return !userInfo.data.top11.includes(player._id);
+      });
+      setNoTop11(remainingPlayers);
     } catch (error) {
       console.error(error);
     }
   };
-
-  //* Jorge AUXILIO , REFRESH, CONTADOR, PERFIL. 
 
   const addToTop11PlayersList = async (player) => {
     if (top11PlayersList.length < 11 && !isPlayerInTop11(player)) {
@@ -53,19 +51,16 @@ function Top11Players() {
       if (player.playerPosition === "forward" && forwardCount >= 3) {
         return;
       }
-  
+
       try {
-        // Llamada al servicio para guardar el Top11 elegido
         const response = await createTop11Service({
           player: player._id,
           position: player.playerPosition,
         });
         console.log(response, "respuesta del servicio");
-  
-        // Agregar el Top11 a la lista
+
         setTop11PlayersList((prevList) => [...prevList, response.data]);
-        console.log(top11PlayersList, "que estamos almacenando?")
-  
+
         if (player.playerPosition === "goalkeeper") {
           setGoalkeeperCount((count) => count + 1);
         } else if (player.playerPosition === "defense") {
@@ -75,12 +70,14 @@ function Top11Players() {
         } else if (player.playerPosition === "forward") {
           setForwardCount((count) => count + 1);
         }
+
+        const top11response = await getTop11Service();
+        setTop11PlayersList(top11response.data);
       } catch (error) {
         console.error(error);
       }
     }
   };
-  
 
   const isPlayerInTop11 = (player) => {
     return top11PlayersList.some((p) => p.player._id === player._id);
@@ -93,18 +90,10 @@ function Top11Players() {
   return (
     <div>
       <h3>Our 11 chosen</h3>
-      <p>
-        {goalkeeperCount} goalkeeper(s) selected (max: 1)
-      </p>
-      <p>
-        {defenseCount} defense(s) selected (max: 4)
-      </p>
-      <p>
-        {midfielderCount} midfielder(s) selected (max: 3)
-      </p>
-      <p>
-        {forwardCount} forward(s) selected (max: 3)
-      </p>
+      <p>{goalkeeperCount} goalkeeper(s) selected (max: 1)</p>
+      <p>{defenseCount} defense(s) selected (max: 4)</p>
+      <p>{midfielderCount} midfielder(s) selected (max: 3)</p>
+      <p>{forwardCount} forward(s) selected (max: 3)</p>
 
       {top11PlayersList.map((eachPlayer) => (
         <div className="top11-card" key={eachPlayer._id}>
@@ -124,7 +113,9 @@ function Top11Players() {
       <div className="player-list-in-top11">
         {noTop11.map((eachPlayer) => (
           <div className="player-card" key={eachPlayer._id}>
-            <h3>{eachPlayer.name}, {eachPlayer.age}</h3>
+            <h3>
+              {eachPlayer.name}, {eachPlayer.age}
+            </h3>
             <p>{eachPlayer.playerPosition}</p>
             <p>{eachPlayer.skillfulLeg}</p>
             {!isPlayerInTop11(eachPlayer) && (
@@ -141,4 +132,4 @@ function Top11Players() {
   );
 }
 
-export default Top11Players
+export default Top11Players;
