8c6bb5b3f0d27e54f2a4c7a61acb2df8bc684d84
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-06-07 15:28:59 +0200

    deploy push

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import CreatePlayer from './pages/players/CreatePlayer';
 import Footer from './components/Footer';
 import DeletePlayer from './pages/players/DeletePlayer';
 import EditPlayer from './pages/players/EditPlayer';
+import EditProfile from './pages/users/EditProfile';
 
 function App() {
   return (
@@ -30,13 +31,14 @@ function App() {
     <Route path="/auth/signup" element={ <Signup /> } />
     <Route path="/auth/login" element={ <Login /> } />
     <Route path="/user/profile" element={ <IsPrivate> <Profile /> </IsPrivate> } />
+    <Route path="/user/profile/edit" element={ <IsPrivate> <EditProfile /> </IsPrivate> } />
     <Route path="/players/list" element={ <IsPrivate> <List /> </IsPrivate> } />
     <Route path="/top11/createTop11" element={ <IsPrivate> <Top11Players /> </IsPrivate> } />
     <Route path="/players/new-player" element={ <IsPrivate> <CreatePlayer /> </IsPrivate> } />
     <Route path="/players/:playerId/details" element={ <IsPrivate> <Details /> </IsPrivate> } />
     <Route path="/players/:playerId/delete" element={ <IsPrivate> <DeletePlayer /> </IsPrivate> } />
     <Route path="/players/:playerId/edit" element={ <IsPrivate> <EditPlayer /> </IsPrivate> } />
-    <Route path="/top11/createTop11" element={ <IsPrivate> <Top11Players /> </IsPrivate> } />
+    {/* <Route path="/top11/createTop11" element={ <IsPrivate> <Top11Players /> </IsPrivate> } /> */}
     {/* <Route path="/players/:playerId/like" element={ <IsPrivate> <Details /> </IsPrivate> } />
     <Route path="/players/:playerId/unLike" element={ <IsPrivate> <Details /> </IsPrivate> } /> */}
 
