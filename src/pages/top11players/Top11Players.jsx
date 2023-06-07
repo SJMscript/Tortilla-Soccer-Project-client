86d056ebb3f51b6ea8a7e1728ec59558cbc919c9
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-06-07 20:00:04 +0200

    JORGE AUXILIO

--- src/pages/top11players/Top11Players.jsx
+++ src/pages/top11players/Top11Players.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { playersListService } from "../../services/players.services";
 import { useNavigate } from "react-router-dom";
-import { createTop11Service } from "../../services/top11Players.services";
+import { createTop11Service, getTop11Service } from "../../services/top11Players.services";
+import { profileService } from "../../services/profile.services";
 
 function Top11Players() {
   const [playersList, setPlayersList] = useState([]);
@@ -10,6 +11,7 @@ function Top11Players() {
   const [defenseCount, setDefenseCount] = useState(0);
   const [midfielderCount, setMidfielderCount] = useState(0);
   const [forwardCount, setForwardCount] = useState(0);
+  const [ noTop11, setNoTop11 ] = useState([])
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,39 +23,21 @@ function Top11Players() {
       const response = await playersListService();
       console.log(response, "response");
       setPlayersList(response.data);
+      const top11response = await getTop11Service()
+      console.log(top11response)
+      setTop11PlayersList(top11response.data)
+      const userInfo = await profileService();
+      console.log(userInfo, "userinfo")
+      const remainingPlayers = response.data.filter(player => {
+        return !userInfo.data.top11.includes(player._id)
+      })
+      setNoTop11(remainingPlayers)
     } catch (error) {
       console.error(error);
     }
   };
 
-/*   const addToTop11PlayersList = (player) => {
-    if (top11PlayersList.length < 11 && !isPlayerInTop11(player)) {
-      if (player.playerPosition === "goalkeeper" && goalkeeperCount >= 1) {
-        return;
-      }
-      if (player.playerPosition === "defense" && defenseCount >= 4) {
-        return;
-      }
-      if (player.playerPosition === "midfielder" && midfielderCount >= 3) {
-        return;
-      }
-      if (player.playerPosition === "forward" && forwardCount >= 3) {
-        return;
-      }
-
-      setTop11PlayersList((prevList) => [...prevList, player]);
-
-      if (player.playerPosition === "goalkeeper") {
-        setGoalkeeperCount((count) => count + 1);
-      } else if (player.playerPosition === "defense") {
-        setDefenseCount((count) => count + 1);
-      } else if (player.playerPosition === "midfielder") {
-        setMidfielderCount((count) => count + 1);
-      } else if (player.playerPosition === "forward") {
-        setForwardCount((count) => count + 1);
-      }
-    }
-  }; */
+  //* Jorge AUXILIO , REFRESH, CONTADOR, PERFIL. 
 
   const addToTop11PlayersList = async (player) => {
     if (top11PlayersList.length < 11 && !isPlayerInTop11(player)) {
@@ -99,7 +83,7 @@ function Top11Players() {
   
 
   const isPlayerInTop11 = (player) => {
-    return top11PlayersList.some((p) => p._id === player._id);
+    return top11PlayersList.some((p) => p.player._id === player._id);
   };
 
   const navigateToCreateTop11 = () => {
@@ -125,20 +109,20 @@ function Top11Players() {
       {top11PlayersList.map((eachPlayer) => (
         <div className="top11-card" key={eachPlayer._id}>
           <li>
-            <h3>{eachPlayer.name}</h3>
-            {eachPlayer.imageUrl && (
+            <h3>{eachPlayer.player.name}</h3>
+            {eachPlayer.player.imageUrl && (
               <div>
-                <img src={eachPlayer.imageUrl} alt="Player" width={200} />
+                <img src={eachPlayer.player.imageUrl} alt="Player" width={200} />
               </div>
             )}
-            <p>{eachPlayer.playerPosition}</p>
+            <p>{eachPlayer.player.playerPosition}</p>
           </li>
         </div>
       ))}
 
       <h3>All Players</h3>
       <div className="player-list-in-top11">
-        {playersList.map((eachPlayer) => (
+        {noTop11.map((eachPlayer) => (
           <div className="player-card" key={eachPlayer._id}>
             <h3>{eachPlayer.name}, {eachPlayer.age}</h3>
             <p>{eachPlayer.playerPosition}</p>
@@ -158,159 +142,3 @@ function Top11Players() {
 }
 
 export default Top11Players
-
-
-/* import { useState, useEffect } from "react";
-import { playersListService } from "../../services/players.services";
-import { getTop11PlayersService } from "../../services/top11Players.services";
-import { useNavigate } from "react-router-dom";
-
-function Top11Players() {
-  const [playersList, setPlayersList] = useState([]);
-  const [top11PlayersList, setTop11PlayersList] = useState([]);
-  const [goalkeeperCount, setGoalkeeperCount] = useState(0);
-  const [defenseCount, setDefenseCount] = useState(0);
-  const [midfielderCount, setMidfielderCount] = useState(0);
-  const [forwardCount, setForwardCount] = useState(0);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    getPlayersListData();
-    getTop11PlayersData();
-  }, []);
-
-  const getPlayersListData = async () => {
-    try {
-      const response = await playersListService();
-      setPlayersList(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const getTop11PlayersData = async () => {
-    try {
-      const response = await getTop11PlayersService();
-      setTop11PlayersList(response.data);
-      updatePositionCounts(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const updatePositionCounts = (top11Players) => {
-    let goalkeeperCount = 0;
-    let defenseCount = 0;
-    let midfielderCount = 0;
-    let forwardCount = 0;
-
-    top11Players.forEach((player) => {
-      switch (player.playerPosition) {
-        case "goalkeeper":
-          goalkeeperCount++;
-          break;
-        case "defense":
-          defenseCount++;
-          break;
-        case "midfielder":
-          midfielderCount++;
-          break;
-        case "forward":
-          forwardCount++;
-          break;
-        default:
-          break;
-      }
-    });
-
-    setGoalkeeperCount(goalkeeperCount);
-    setDefenseCount(defenseCount);
-    setMidfielderCount(midfielderCount);
-    setForwardCount(forwardCount);
-  };
-
-  const addToTop11PlayersList = (player) => {
-    if (top11PlayersList.length < 11 && !isPlayerInTop11(player)) {
-      if (player.playerPosition === "goalkeeper" && goalkeeperCount >= 1) {
-        return;
-      }
-      if (player.playerPosition === "defense" && defenseCount >= 4) {
-        return;
-      }
-      if (player.playerPosition === "midfielder" && midfielderCount >= 3) {
-        return;
-      }
-      if (player.playerPosition === "forward" && forwardCount >= 3) {
-        return;
-      }
-
-      setTop11PlayersList((prevList) => [...prevList, player]);
-      updatePositionCounts([...top11PlayersList, player]);
-    }
-  };
-
-  const isPlayerInTop11 = (player) => {
-    return top11PlayersList.some((p) => p._id === player._id);
-  };
-
-  const navigateToCreateTop11 = () => {
-    navigate("/createTop11");
-  };
-
-  return (
-    <div>
-      <h3>Our 11 chosen</h3>
-      <p>
-        {goalkeeperCount} goalkeeper(s) selected (max: 1)
-      </p>
-      <p>
-        {defenseCount} defense(s) selected (max: 4)
-      </p>
-      <p>
-        {midfielderCount} midfielder(s) selected (max: 3)
-      </p>
-      <p>
-        {forwardCount} forward(s) selected (max: 3)
-      </p>
-
-      {top11PlayersList.map((eachPlayer) => (
-        <div className="top11-card" key={eachPlayer._id}>
-          <li>
-            <h3>{eachPlayer.name}</h3>
-            {eachPlayer.imageUrl && (
-              <div>
-                <img src={eachPlayer.imageUrl} alt="Player" width={200} />
-              </div>
-            )}
-            <p>{eachPlayer.playerPosition}</p>
-          </li>
-        </div>
-      ))}
-
-      <h3>All Players</h3>
-      <div className="player-list-in-top11">
-        {playersList.map((eachPlayer) => (
-          <div className="player-card" key={eachPlayer._id}>
-            <h3>{eachPlayer.name}, {eachPlayer.age}</h3>
-            <p>{eachPlayer.playerPosition}</p>
-            <p>{eachPlayer.skillfulLeg}</p>
-            {!isPlayerInTop11(eachPlayer) && (
-              <button onClick={() => addToTop11PlayersList(eachPlayer)}>
-                Add to Top 11
-              </button>
-            )}
-          </div>
-        ))}
-      </div>
-
-      <button onClick={navigateToCreateTop11}>Go to Create Top 11</button>
-    </div>
-  );
-}
-
-export default Top11Players;
- */
-
-
-
-
--- src/services/top11Players.services.js
+++ src/services/top11Players.services.js
@@ -1,11 +1,16 @@
 import service from "./config.services";
 
+const getTop11Service = () => {
+    return service.get(`/top11/createTop11`)
+}
+
 
 const createTop11Service = (top11Data) => {
     return service.post(`/top11/createTop11`, top11Data)
 }
 
 export {
+    getTop11Service,
     createTop11Service,
 }
 
