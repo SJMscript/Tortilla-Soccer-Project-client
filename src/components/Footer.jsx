32cea59677595045f327c0dc268d4bd1bd1af575
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-06-08 19:47:39 +0200

    deploy test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,13 @@
 function Footer() {
   return (
     <div className="footer-div">
-    <p>Created by Juan Diego & Santiago</p>
-    <p><a href="https://github.com/SJMscript?tab=repositories&q=tortilla&type=&language=&sort=" target="_blank">Tortilla Soccer 2023 &copy;</a></p>
+    <a href="https://www.linkedin.com/in/juan-diego-arocha-8a2556182/" target="_blank" className="link-footer">Juan Diego </a>
+
+
+    <p><a href="https://github.com/SJMscript?tab=repositories&q=tortilla&type=&language=&sort=" target="_blank" className="link-footer">Tortilla Soccer 2023 ®</a></p>
+
+    
+    <a href="https://www.linkedin.com/in/santiagomassa/" target="_blank" className="link-footer">Santiago </a>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-
+import styles from "../css/home.module.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+      <div className={styles.home}>
+      <div className="home-div">
+        <h1 className={styles.h1TitleHome}>Tortilla Soccer</h1>
+        <h3 className={styles.h3SubtitleHome}>Created by Juan Diego y Santiago</h3>
+      </div>
+        <section>
+          <img src="/images/españa.jpg" alt="esp" />
+          <img src="/images/españa1.jpg" alt="esp" />
+          <img src="/images/españa2.jpg" alt="esp" />
+          <img src="/images/españa3.jpg" alt="esp" />
+          <img src="/images/españa4.jpg" alt="esp" />
+          <img src="/images/españa5.jpg" alt="esp" />
+          <img src="/images/españa6jpg.jpg" alt="esp" />
+        </section>
+        </div>
+  );
 }
 
-export default Home
+export default Home;
